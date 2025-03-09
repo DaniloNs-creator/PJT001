d9635f9d023720a045627da2bdc145bfcd5ac388
DaniloNs-creator <sousadanilo601@outlook.com>
2025-03-08 21:43:57 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -3,6 +3,12 @@ import pandas as pd
 import numpy as np
 from io import BytesIO
 
+import subprocess
+# Atualizando o pip antes de qualquer outra instalação
+subprocess.check_call(["python", '-m', 'pip', 'install', '--upgrade', 'pip'])
+
+subprocess.check_call(["python", '-m', 'pip', 'install', 'xlsxwriter'])
+
 # Certifique-se de que o módulo xlsxwriter está instalado
 try:
     import xlsxwriter
